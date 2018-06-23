b978f866cfe8d796a55d66483eac8dc0ac0c80b0
lyon <rethinkt@163.com>
2018-06-23 22:41:50 +0800

    update some file

--- python-base/02statement/demo01.py
+++ python-base/02statement/demo01.py
@@ -10,6 +10,8 @@ if 要判断的条件:
 
 a = 100
 
+b = 200
+
 print("------简单if判断------")
 
 if a >= 100:
@@ -29,70 +31,14 @@ elif a >= 2:
 else:
     print("条件成立走c分支")
 
+print("------嵌套if判断------")
+if b == 200:
+    print("条件成立")
+    if a < 100:
+        print("内部条件成立")
+    else:
+        print("内部条件不成立")
+else:
+    print("条件不成立")
 
-# import sys
-# class Dog:
-#     def set_age(self,new_age):
-#         if new_age < 100 and new_age > 0:
-#             self.age = new_age
-#         else:
-#             self.age=1
-#     def __init__(self):
-#         print("----init------")
-#
-#     def get_age(self):
-#         return self.age
-#
-#
-#     def test1(self):
-#         print("----test1----------")
-#         self.__test2()
-#     def __test2(self):
-#         print("-----test2---------")
-#
-#     def __del__(self):
-#         print("对象的del方法！")
-#
-#     def my(self):
-#         print("dog my method")
-# dog = Dog()
-# dog.set_age(102)
-#
-# age = dog.get_age()
-# print(age)
-#
-# dog.test1()
-# dog.__init__()
-# print(sys.getrefcount(dog))
-# del dog
-#
-# class T:
-#     pass
-# t=T()
-# tt = t
-# # del tt
-#
-# class D(Dog):
-#     num = 0
-#     def my(self):
-#         print("this my method")
-#         Dog.my(self)
-#         super().my()
-#
-#     @classmethod
-#     def the(cls):
-#         cls.num = 12
-#         print("hello")
-#     @staticmethod
-#     def abc():
-#         print("static method")
-#     pass
-#
-# ddd = D()
-# ddd.test1()
-# ddd.my()
-# D.the()
-#
-# print(D.__mro__)
-#
-# test.test
+
--- python-base/02statement/demo02.py
+++ python-base/02statement/demo02.py
@@ -1,59 +1,91 @@
+# coding:utf-8
 
-
-class CarStore(object):
-    def __init__(self):
-        self.factory = Factory()
-    def order(self, tpye):
-        return self.factory.select_car_by_car_type(type)
-    def __del__(self):
-        print("del")
-    def __str__(self):
-        print("str 对象的描述信息")
-    def __new__(cls):
-        print(id(cls))
-        print("--new method--")
-        return object.__new__(cls)
-
-
-class Factory(object):
-    def select_car_by_car_type(self,car_type):
-            if car_type == "qiche":
-                return Car()
-            elif car_type == "benci":
-                return Benci()
-            elif car_type == "baoma":
-                return Baoma()
-
-class Car(object):
-    def music(self):
-        print("music")
-    def stop(self):
-        print("stop")
-    def move(self):
-        print("move")
-
-class Benci(Car):
-    pass
-
-class Baoma(Car):
-    pass
-
-
-car_store = CarStore()
-# car = car_store.order("baoma")
-# car.move()
-# car.music()
-# car.stop()
-
-
-print(id(CarStore))
+"""
+python 循环语句
 
 
 
+"""
+
+i = 0
+while i < 10:
+    print("while循环,这是第%d次循环" % i)
+    i += 1
+
+x = 1
+sum = 0
+
+while x <= 100:
+    sum += x
+    x += 1
+print("1~100的累积和为:%d" % sum)
+
+y = 1
+result = 0
+while y <= 100:
+    if y % 2 == 0:
+        result += y
+    y += 1
+print("1~100的累积和为:%d" % result)
+
+a = 1
+while a <= 5:
+    j = 1
+    while j <= a:
+        print("*", end='')
+        j += 1
+    print("\n")
+    a += 1
+
+b = 1
+while b <= 9:
+    c = 1
+    while c <= b:
+        print("%d*%d=%-2d " % (c, b, b * c), end='')
+        c += 1
+    print("\n")
+    b += 1
+
+print("----for循环----")
+
+name = 'hello world'
+
+for e in name:
+    print(e)
+
+name = ''
+for d in name:
+    print(d)
+else:
+    print("没有数据")
+
+print("----break----")
+for f in 'hello world':
+    if f == ' ':
+        break
+    print(f)
+
+print("----continue----")
+for g in 'hello world':
+    if g == ' ':
+        continue
+    print(g)
 
 
+h = 0
+while h < 10:
+    h = h + 1
+    print('----')
+    if h == 5:
+        break
+    print(h)
 
-
-
+r = 0
+while r < 10:
+    r = r + 1
+    print('----')
+    if r == 5:
+        continue
+    print(r)
 
 
