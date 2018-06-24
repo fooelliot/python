a61af642dceda3ef60147de11297b0268037c0c8
lyon <rethinkt@163.com>
2018-06-24 09:53:35 +0800

    update some code

--- python-base/02statement/03function.py
+++ python-base/02statement/03function.py
@@ -1,10 +1,168 @@
+
 # coding:utf-8
+
 """
 python 函数
 def 函数名():
     代码
-定义了函数之后，就相当于有了一个具有某些功能的代码，想要让这些代码能够执行，需要调用它
 
+定义了函数之后，就相当于有了一个具有某些功能的代码，想要让这些代码能够执行，需要调用它
 调用函数很简单的，通过 函数名() 即可完成调用
+定义时小括号中的参数，用来接收参数用的，称为 “形参”
+调用时小括号中的参数，用来传递给函数用的，称为 “实参”
+想要在函数中把结果返回给调用者，需要在函数中使用return
+
+
+局部变量，就是在函数内部定义的变量
+不同的函数，可以定义相同的名字的局部变量，但是各用个的不会产生影响
+局部变量的作用，为了临时保存数据需要在函数中定义变量来进行存储，这就是它的作用
+
+如果一个变量，既能在一个函数中使用，也能在其他的函数中使用，这样的变量就是全局变量
 
 """
+
+
+# 定义一个函数，能够完成打印信息的功能
+def print_info():
+    print('------------------------------------')
+    print('人生苦短，我用Python')
+    print('------------------------------------')
+
+
+# 定义完函数后，函数是不会自动执行的，需要调用它才可以
+print_info()
+
+
+# help(print_info())
+
+def number_sum(a, b):
+    return a + b
+
+
+print(number_sum(12, 23))
+
+
+def number_max(a, b):
+    if a > b:
+        print("%d" % a)
+    else:
+        print("%d" % b)
+
+
+number_max(12, 23)
+
+
+# 一个函数里面又调用了另外一个函数，这就是所谓的函数嵌套调用
+def test_b():
+    print('---- testB start----')
+    print('这里是testB函数执行的代码...(省略)...')
+    print('---- testB end----')
+
+
+def test_a():
+    print('---- testA start----')
+    test_b()
+    print('---- testA end----')
+
+
+test_a()
+
+c = 100
+
+"""
+    在函数外边定义的变量叫做全局变量
+    全局变量能够在所有的函数中进行访问
+    如果在函数中修改全局变量，那么就需要使用global进行声明，否则出错
+    如果全局变量的名字和局部变量的名字相同，那么使用的是局部变量的，小技巧强龙不压地头蛇
+    
+    在函数中不使用global声明全局变量时不能修改全局变量的本质是不能修改全局变量的指向，即不能将全局变量指向新的数据。
+    对于不可变类型的全局变量来说，因其指向的数据不能修改，所以不使用global时无法修改全局变量。
+    对于可变类型的全局变量来说，因其指向的数据可以修改，所以不使用global时也可修改全局变量。
+    
+    
+    
+    
+"""
+
+
+def test_c():
+    # c = 200
+    global c
+    print("修改之前:%d" % c)
+    c = 300
+    print("修改之后:%d" % c)
+
+
+test_c()
+
+
+def divid(a, b):
+    shang = a // b
+    yushu = a % b
+    return shang, yushu
+
+
+print(divid(12, 4))
+
+
+# 注意：带有默认值的参数一定要位于参数列表的最后面。
+def test_d(name, age=35):
+    # 打印任何传入的字符串
+    print("Name: ", name)
+    print("Age ", age)
+
+
+print(test_d("james"))
+print(test_d("kobe", 34))
+
+
+# 不定长参数
+# 加了星号（*）的变量args会存放所有未命名的变量参数，args为元组；而加**的变量kwargs会存放命名参数，即形如key=value的参数， kwargs为字典。
+def fun(a, b, *args, **kwargs):
+    """可变参数演示示例"""
+    print("a =", a)
+    print("b =", b)
+    print("args =", args)
+    print("kwargs: ")
+    for key, value in kwargs.items():
+        print(key, "=", value)
+
+
+fun(1, 2, 3, 4, 5, m=6, n=7, p=8)  # 注意传递的参数对应
+
+c = (3, 4, 5)
+d = {"m": 6, "n": 7, "p": 8}
+
+fun(1, 2, *c, **d)  # 注意元组与字典的传参方式
+
+"""
+匿名函数
+用lambda关键词能创建小型匿名函数。这种函数得名于省略了用def声明函数的标准步骤。
+
+lambda函数的语法只包含一个语句，如下：
+"""
+
+sum = lambda arg1, arg2: arg1 + arg2
+# 调用sum函数
+print("Value of total : ", sum(10, 20))
+
+print("Value of total : ", sum(20, 20))
+
+
+def fun(a, b, opt):
+    print("a =", a)
+    print("b =", b)
+    print("result =", opt(a, b))
+
+
+fun(1, 2, lambda x, y: x + y)
+
+
+stus = [
+    {"name": "zhangsan", "age": 18},
+    {"name": "lisi", "age": 19},
+    {"name": "wangwu", "age": 17}
+]
+
+stus.sort(key=lambda x: x['name'])
+print(stus)
---
+++ python-base/02statement/06file.py
@@ -0,0 +1,80 @@
+# coding:utf-8
+
+"""
+python 的文件操作
+在python，使用open函数，可以打开一个已经存在的文件，或者创建一个新文件
+open(文件名，访问模式)
+关闭文件
+close( )
+
+访问模式	    说明
+r	        以只读方式打开文件。文件的指针将会放在文件的开头。这是默认模式。
+w	        打开一个文件只用于写入。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+a	        打开一个文件用于追加。如果该文件已存在，文件指针将会放在文件的结尾。也就是说，新的内容将会被写入到已有内容之后。如果该文件不存在，创建新文件进行写入。
+rb	        以二进制格式打开一个文件用于只读。文件指针将会放在文件的开头。这是默认模式。
+wb	        以二进制格式打开一个文件只用于写入。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+ab	        以二进制格式打开一个文件用于追加。如果该文件已存在，文件指针将会放在文件的结尾。也就是说，新的内容将会被写入到已有内容之后。如果该文件不存在，创建新文件进行写入。
+r+	        打开一个文件用于读写。文件指针将会放在文件的开头。
+w+	        打开一个文件用于读写。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+a+	        打开一个文件用于读写。如果该文件已存在，文件指针将会放在文件的结尾。文件打开时会是追加模式。如果该文件不存在，创建新文件用于读写。
+rb+	        以二进制格式打开一个文件用于读写。文件指针将会放在文件的开头。
+wb+	        以二进制格式打开一个文件用于读写。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+ab+	        以二进制格式打开一个文件用于追加。如果该文件已存在，文件指针将会放在文件的结尾。如果该文件不存在，创建新文件用于读写。
+
+"""
+
+# 写数据(write)
+f = open('../hello.txt', 'w')
+f.write('hello world, i am here!\njames')
+f.close()
+
+# 读数据(read)
+# 如果open是打开一个文件，那么可以不用谢打开的模式，即只写 open('test.txt')
+# 如果使用读了多次，那么后面读取的数据是从上次读完后的位置开始的
+f = open('../hello.txt', 'r')
+content = f.read(5)
+print(content)
+print("-" * 30)
+content = f.read()
+print(content)
+f.close()
+
+# 读数据（readlines）
+# 就像read没有参数时一样，readlines可以按照行的方式把整个文件中的内容进行一次性读取，并且返回的是一个列表，其中每一行的数据为一个元素#
+
+f = open('../hello.txt', 'r')
+content = f.readlines()
+print(type(content))
+i = 1
+for temp in content:
+    print("%d:%s" % (i, temp))
+    i += 1
+
+f.close()
+
+
+oldFileName = input("请输入要拷贝的文件名字:")
+
+oldFile = open(oldFileName, 'r')
+
+# 如果打开文件
+if oldFile:
+
+    # 提取文件的后缀
+    fileFlagNum = oldFileName.rfind('.')
+    if fileFlagNum > 0:
+        fileFlag = oldFileName[fileFlagNum:]
+
+    # 组织新的文件名字
+    newFileName = oldFileName[:fileFlagNum] + '[复件]' + fileFlag
+
+    # 创建新文件
+    newFile = open(newFileName, 'w')
+
+    # 把旧文件中的数据，一行一行的进行复制到新文件中
+    for lineContent in oldFile.readlines():
+        newFile.write(lineContent)
+
+    # 关闭文件
+    oldFile.close()
+    newFile.close()
